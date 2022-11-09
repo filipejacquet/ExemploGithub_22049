7503917846a35ed75173c3505300353af0a899a9
filipejacquet <7412784@formacao.iefp.pt>
2022-11-09 16:37:11 +0000

    Primeiro Repositório Alteração Nome

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Turma 22049");
-        System.out.println("Ligia");
+        System.out.println("Filipe");
     }
 
 
